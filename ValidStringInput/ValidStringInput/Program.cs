f420787bc65ee4108e45e34d1aa2b8a713ffa2b4
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-10 16:14:18 +0200

    Valid string input
    An exercise from the course

---
+++ ValidStringInput/ValidStringInput/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ValidStringInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string readResult = "";
+            bool validRole = false;
+            string formattedResult;
+            
+            do
+            {
+                Console.Write("Enter your role name (Administrator, Manager, or User): ");
+                readResult = Console.ReadLine();
+
+                if (readResult != null)
+                {
+                    formattedResult = readResult.Trim().ToLower();
+                    if (formattedResult == "administrator" || formattedResult == "manager" || formattedResult == "user")
+                    {
+                        validRole = true;
+                    }
+                }
+
+                if (!validRole)
+                {
+                    Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid.");
+                }
+
+            } while (!validRole);
+
+            Console.WriteLine($"Your input value ({readResult.Trim()}) has been accepted.");
+        }
+    }
+}
