a0b56ec01316cef7ac07908a5a1f59e804180aca
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-10 16:32:21 +0200

    Process string array
    An exercise form the course

---
+++ ProcessStringArray/ProcessStringArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProcessStringArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+            int periodLocation;
+
+            foreach (string myString in myStrings)
+            {
+                string str = myString;
+                periodLocation = myString.IndexOf('.');
+
+                while (periodLocation > -1)
+                {
+                    Console.WriteLine(str.Substring(0, periodLocation));
+                    str = str.Remove(0, periodLocation + 2);
+                    periodLocation = str.IndexOf('.');
+                }
+
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
