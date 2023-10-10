5ae4f07f34756c21145ae5e4e1c4555a2e090243
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-10 14:45:37 +0200

    FizzBuzz
    A classic exercise

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 25; ++i)
+            {
+                string postfix = "";
+
+                if (i % 3 == 0)
+                    postfix += "Fizz";
+
+                if (i % 5 == 0)
+                    postfix += "Buzz";
+
+                Console.WriteLine($"{i} {postfix}");
+            }
+        }
+    }
+}
