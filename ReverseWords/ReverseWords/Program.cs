a4f8dfab52961a7b2c1f3c748d936e605a83af20
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-12 21:25:05 +0200

    Reverse words in a string
    An exercise from the course

---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReverseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangram = "The quick brown fox jumps over the lazy dog";
+            string[] words = pangram.Split(' ');
+
+            for (int i = 0; i < words.Length; ++i)
+            {
+                char[] letters = words[i].ToCharArray();
+                Array.Reverse(letters);
+                words[i] = new string(letters);
+            }
+
+            string reversedPangram = String.Join(" ", words);
+            Console.WriteLine(reversedPangram);
+        }
+    }
+}
