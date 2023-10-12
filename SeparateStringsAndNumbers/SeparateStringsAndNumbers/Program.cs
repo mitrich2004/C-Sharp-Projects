ecf95dfb1788f13541697b71a3f318588325e3cc
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-12 20:44:30 +0200

    Separate string and numbers
    An exercise from the course

---
+++ SeparateStringsAndNumbers/SeparateStringsAndNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SeparateStringsAndNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+            string message = "";
+            decimal total = 0.0m;
+            decimal tmp;
+
+            foreach (string value in values)
+            {
+                if (decimal.TryParse(value, out tmp))
+                {
+                    total += tmp;
+                }
+                else
+                {
+                    message += value;
+                }
+            }
+
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
