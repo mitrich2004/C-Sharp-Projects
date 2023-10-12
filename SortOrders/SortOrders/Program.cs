4c67219ad9cb74b4dab259eec62c548512652b6e
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-12 21:33:30 +0200

    Sort orders
    An exercise from the course

---
+++ SortOrders/SortOrders/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SortOrders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+            string[] orders = orderStream.Split(',');
+            Array.Sort(orders);
+
+            foreach (string order in orders)
+            {    
+                if (order.Length != 4)
+                {
+                    Console.WriteLine($"{order}\t- Error");
+                }
+                else
+                {
+                    Console.WriteLine(order);
+                }
+            }
+        }
+    }
+}
