78fd68d842ce446318b8df6e74d2d7398c06fa1a
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-14 16:17:27 +0200

    Get the text inside the brackets
    An exercise from the course

---
+++ TextInBrackets/TextInBrackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TextInBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+            char[] openSymbols = { '[', '{', '(' };
+
+            int closingPosition = 0;
+
+            while (true)
+            {
+                int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+                if (openingPosition == -1)
+                {
+                    break;
+                }
+
+                char openingBracket = message[openingPosition];
+                char closingBracket = ' ';
+
+                switch (openingBracket)
+                {
+                    case '[':
+                        closingBracket = ']';
+                        break;
+                    case '(':
+                        closingBracket = ')';
+                        break;
+                    case '{':
+                        closingBracket = '}';
+                        break;
+                }
+
+                openingPosition += 1;
+                closingPosition = message.IndexOf(closingBracket, openingPosition);
+                int length = closingPosition - openingPosition;
+
+                Console.WriteLine(message.Substring(openingPosition, length));
+            }
+        }
+    }
+}
