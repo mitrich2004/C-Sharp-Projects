dfcc44f0a7dd263f3efe44a1d05bdfe2622a4cc0
Sergey Miachkov <miachkov2004@gmail.com>
2023-10-16 16:11:05 +0200

    Check if the word is palidrome
    An exercise from the course

---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
+
+            Console.WriteLine("Is it a palindrome?");
+            foreach (string word in words)
+            {
+                Console.WriteLine($"{word}: {IsPalindrome(word)}");
+            }
+        }
+        static bool IsPalindrome(string word)
+        {
+            int left = 0;
+            int right = word.Length - 1;
+
+            while (left < right)
+            {
+                if (word[left] != word[right])
+                {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
